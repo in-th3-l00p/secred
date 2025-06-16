b95ff9e7c69bb251e80dbe29fe1804cd38531843
Tisca Catalin <admin@tiscacatalin.com>
2025-06-16 10:28:12 +0300

    modified main.rs

--- temp-service/src/main.rs
+++ temp-service/src/main.rs
@@ -1,32 +1,35 @@
-use std::convert::Infallible;
 use std::net::SocketAddr;
+use std::convert::Infallible;
 
 use http_body_util::Full;
+use tokio::net::TcpListener;
 use hyper::body::Bytes;
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
-use tokio::net::TcpListener;
 
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+async fn service(req: Request<hyper::body::Incoming>) ->
+    Result<Response<Full<Bytes>>, Infallible>
+{
+    Ok(Response::new(Full::new(Bytes::from("hello"))))
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let listener = TcpListener::bind(addr).await?;
+
     loop {
         let (stream, _) = listener.accept().await?;
         let io = TokioIo::new(stream);
-
         tokio::task::spawn(async move {
-            if let Err(err) = http1::Builder::new()
-                .serve_connection(io, service_fn(hello))
-                .await
+            if let Err(err) = 
+                http1::Builder::new()
+                    .serve_connection(io, service_fn(service))
+                    .await
             {
-                eprintln!("Error serving connection: {:?}", err);
+                eprintln!("error serving: {:?}", err);
             }
         });
     }
